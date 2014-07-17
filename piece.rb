5682d2f678aa3ce42cc9bfca5eca37364c77e268
Noah Gordon <tortuga.ng@gmail.com>
2014-07-17 09:35:18 -0400

    Add move_diffs for regular pieces and king. Implement perform_slide method.

--- piece.rb
+++ piece.rb
@@ -2,13 +2,21 @@
 
 class Piece
 
+  attr_accessor :board, :position
+  attr_reader :color
+
   def initialize(position, color, board)
     @position, @color, @board = position, color, board
     @king = false
   end
 
   def perform_slide(target)
+    raise InvalidMoveError unless @board[target].nil?
+    raise InvalidMoveError unless valid_slides.include? target
 
+    board[position] = nil
+    board[target] = self
+    self.position = target
   end
 
   def perform_jump(target)
@@ -25,6 +33,17 @@ class Piece
 
   def is_king?
     @king
+  end
+
+  def valid_slides
+    move_diffs.each_with_object do |(dx, dy), []|
+      [] << [@position[0] + dx, @position[1] + dy]
+    end
+  end
+
+  def move_diffs
+    diffs = [[forward_dir, -1], [forward_dir, 1]]
+    diffs += [[backward_dir, -1], [backward_dir, 1]] if is_king?
   end
 
   def forward_dir
