89974b226bde0a7b0960a2cccc12adda45651892
Noah Gordon <tortuga.ng@gmail.com>
2014-07-17 09:27:40 -0400

    Add basic method skeleton for piece class.

--- piece.rb
+++ piece.rb
@@ -1,1 +1,38 @@
 # encoding: utf-8
+
+class Piece
+
+  def initialize(position, color, board)
+    @position, @color, @board = position, color, board
+    @king = false
+  end
+
+  def perform_slide(target)
+
+  end
+
+  def perform_jump(target)
+
+  end
+
+  def eligible_for_promotion?
+    # not a king, in the right row for the right color
+  end
+
+  def promote
+
+  end
+
+  def is_king?
+    @king
+  end
+
+  def forward_dir
+    @color == :light ? -1 : 1
+  end
+
+  def backward_dir
+    @color == :light ? 1 : -1
+  end
+
+end
