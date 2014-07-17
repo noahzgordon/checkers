283f20f6afd0046e95d02f1a3d3b6648b45c8fd8
Noah Gordon <tortuga.ng@gmail.com>
2014-07-17 11:30:27 -0400

    Start to build game class.

--- game.rb
+++ game.rb
@@ -1,1 +1,22 @@
 require_relative 'board'
+
+class Game
+
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
+  def play
+
+    until won?
+
+    end
+  end
+
+  def won?
+    board.pieces.all? { |piece| piece.color == :light || piece.color == :dark }
+  end
+
+end
