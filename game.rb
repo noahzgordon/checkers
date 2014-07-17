0dab65e48a8fcaa5b59f7a285429a108f85f5aba
Noah Gordon <tortuga.ng@gmail.com>
2014-07-17 11:44:32 -0400

    Finish first implementation of game class.

--- game.rb
+++ game.rb
@@ -1,6 +1,27 @@
 require_relative 'board'
 
 class Game
+  COLS = {
+      "a" => 0,
+      "b" => 1,
+      "c" => 2,
+      "d" => 3,
+      "e" => 4,
+      "f" => 5,
+      "g" => 6,
+      "h" => 7
+    }
+
+    ROWS = {
+      "1" => 7,
+      "2" => 6,
+      "3" => 5,
+      "4" => 4,
+      "5" => 3,
+      "6" => 2,
+      "7" => 1,
+      "8" => 0
+    }
 
   attr_reader :board
 
@@ -14,10 +35,13 @@ class Game
     until won?
       puts "#{@turn.to_s.capitalize}'s turn!\n"
 
-      @board.display
+      board.display
 
       start, moves = get_input
 
+      board[start].perform_moves(moves)
+
+      @turn == :light ? @turn = :dark : @turn = :light
     end
 
     puts "Game over!"
@@ -25,7 +49,14 @@ class Game
 
   def get_input
     puts "What piece would you like to move?"
-    piece = gets.chomp.to_i
+    coords = gets.chomp.downcase
+    piece_pos = [ROWS[coords[0]], COLS[coords[1]]]
+
+    puts "Input a move or sequence of moves (separated by commas)."
+    input = gets.chomp.downcase.gsub(' ', '').split(',')
+    sequence = input.map { |pair| [ROWS[pair[0]], COLS[pair[1]]] }
+
+    [start, moves]
   end
 
   def won?
